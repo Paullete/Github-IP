1a124cd52a943387b7101171f0a5263f0c4bd533
paullete <paulettenereah3@gmail.com>
2022-04-18 08:01:19 +0300

    details

--- angularip/src/app/highlight.directive.ts
+++ angularip/src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive ,HostListener,ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
