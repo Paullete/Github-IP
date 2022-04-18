ef8a43e9f5701f128c693f246177c3ac70330b5d
paullete <paulettenereah3@gmail.com>
2022-04-18 07:15:07 +0300

    details

--- angularip/src/app/app.module.ts
+++ angularip/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatePipe } from './date.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { MyReposComponent } from './my-repos/my-repos/my-repos.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { ProfileComponent } from './profile/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatePipe,
+    HighlightDirective,
+    MyReposComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
