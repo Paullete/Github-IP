0f876707cb7f891b8037f2cf2ca3c78bca0d6e2f
paullete <paulettenereah3@gmail.com>
2022-04-18 07:39:31 +0300

    details

--- angularip/src/app/app-routing.module.ts
+++ angularip/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile/profile/profile.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { MyReposComponent } from './my-repos/my-repos/my-repos.component';
+const appRoutes: Routes = [
 
-const routes: Routes = [];
-
+  {path: '', component:ProfileComponent},
+  {path: 'profile', component:ProfileComponent},
+  {path: 'my-repos', component:MyReposComponent}
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class MyRoutingModule { }
